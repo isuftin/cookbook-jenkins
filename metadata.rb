234a1fe35f509ed7825007715269eb7aac258fc8
Suftin, Ivan <isuftin@usgs.gov>
2015-01-25 09:57:41 -0600

    Bumping version and removing stig dependency

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,9 @@
 name             'cida_jenkins'
 maintainer       'Ivan Suftin'
 maintainer_email 'isuftin@usgs.gov'
-license          'usgs_pd'
+license          'Public Domain'
 description      'Installs and configures the Jenkins continuous integration server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
-depends 'stig', '~> 0.2.1'
 depends 'jenkins', '~> 2.2.2'
